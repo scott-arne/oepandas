62d119f5bea28005ec3ed6987688e79a495beefd
Scott Arne Johnson <scott.arne.johnson@gmail.com>
2023-09-15 11:39:51 -0700

    Minor change - added an explicit logger that is configured with level-specific formatting

--- oepandas/__init__.py
+++ oepandas/__init__.py
@@ -1,4 +1,51 @@
+import logging
 from .molecule import MoleculeArray, MoleculeDtype
 from .exception import FileError, UnsupportedFileFormat
 
 __version__ = '0.1.3'
+
+
+########################################################################################################################
+# Configure logging
+########################################################################################################################
+log = logging.getLogger("oepandas")
+
+
+class LevelSpecificFormatter(logging.Formatter):
+    """
+    A logging formatter
+    """
+    NORMAL_FORMAT = "%(message)s"
+    DEBUG_FORMAT = "%(levelname)s: %(message)s"
+
+    def __init__(self):
+        super().__init__(fmt=self.NORMAL_FORMAT, datefmt=None, style='%')
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format a log record for printing
+        :param record: Record to format
+        :return: Formatted record
+        """
+        if record.levelno == logging.DEBUG:
+            self._style._fmt = self.DEBUG_FORMAT
+        else:
+            self._style._fmt = self.NORMAL_FORMAT
+
+        # Call the original formatter class to do the grunt work
+        result = logging.Formatter.format(self, record)
+
+        return result
+
+
+############################
+# Configure the logger
+############################
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+ch.setFormatter(LevelSpecificFormatter())
+log.addHandler(ch)
+
+log.setLevel(logging.INFO)
