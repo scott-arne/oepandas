87b87cfe36586202372cd7c34e088a9caa3b5483
Scott Arne Johnson <scott.johnson@bms.com>
2026-05-06 08:28:50 -0700

    chore: prepare 3.2.7 examples

--- oepandas/__init__.py
+++ oepandas/__init__.py
@@ -16,7 +16,7 @@ from .arrays import (
 from .exception import FileError, UnsupportedFileFormat
 from .pandas_extensions import read_molecule_csv, read_oeb, read_oedb, read_oedu, read_sdf, read_smi
 
-__version__ = '3.2.6'
+__version__ = '3.2.7'
 
 __all__ = [
     "exception",
